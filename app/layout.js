8e04da615d05d34cbcd55ddf91bf7ec5f6b5abf6
HollyWalker1 <c3021739@newcastle.ac.uk>
2024-10-07 17:23:09 +0100

    Set up home page layout.

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,8 @@
-import localFont from "next/font/local";
+import { Fugaz_One, Inter} from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"]})
+const fugaz = Fugaz_One({ subsets: ["latin"], weight:['400']})
 
 export const metadata = {
   title: "TrackR",
@@ -19,21 +11,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   const header = (
-    <header>
-
+    <header className="p-4 sm:p-8 flex items-center justify-between gap-4">
+      <h1 className={'text-base sm:text-lg textGradient ' + fugaz.className}>TrackR</h1>
     </header>
   )
   const footer = (
-    <footer>
-      
+    <footer className="p-4 sm:p-8 grid place-items-center">
+      <p className={'text-indigo-600 ' + fugaz.className}>Created with Node.js</p>
     </footer>
   )
 
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={'w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col text-slate-700'}>
         {header}
         {children}
         {footer}
--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Fugaz_One} from "next/font/google";
+import Button from './Button';
+
+
+const fugaz = Fugaz_One({ subsets: ["latin"], weight:['400']})
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <div className='py-4 md:py-10 flex flex-col gap-4 sm:gap-8'>
+        <h1 className={'text-5xl sm:text-6xl md:text-7xl text-center ' + fugaz.className}><span className="textGradient">TrackR</span> Helps you track your <span className="textGradient">daily</span> spending</h1>
+        <p className='text-lg sm:text-xl md:text-2x w-full mx-auto max-w-[700px] text-center'>Create your spending record to see how much you're spending <span className='font-semibold'>throughout the year</span></p>
+        <div className='grid grid-cols-2 gap-4 w-fit mx-auto'>
+            <Button text="Sign up"/>
+            <Button text="Log In" dark/>
+        </div>
+    </div>
   )
 }
